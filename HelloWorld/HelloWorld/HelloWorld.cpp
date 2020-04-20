13d7fae782482cc49ca15b8969cc150c0538797d
Andy <edrocket734@gmail.com>
2020-04-20 13:22:53 -0400

    Created our first HelloWorld project!

---
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,13 @@
+// HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+	printf("This is on a differnt line!\n");
+	return 0;
+}
+
+
