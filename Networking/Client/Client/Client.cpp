fa6c5b1a361b8213dd3f946268ee0fa208c54081
Andy <edrocket734@gmail.com>
2020-04-23 14:35:13 -0400

    Client and server code for both TCP and UDP connections. You may need to change the IP address in the code to work for your computer.

--- Networking/Client/Client/Client.cpp
+++ Networking/Client/Client/Client.cpp
@@ -1,28 +1,34 @@
 // Client.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS // This turns off the warnings on moving to inet_pton
 #include <stdio.h>
 #include <string.h>
 #include <winsock2.h>
-#include <ws2tcpip.h>
+#include <ws2tcpip.h> // needed for inet_pton
 
-#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "ws2_32.lib") // loads the winsock library
 
+#define UDP 0 // Set this to 1 for UDP, 0 for TCP
 
-#define MESSAGESIZE 1024
+#define MESSAGESIZE 1024 // Resued value for message size
 
 int main()
 {
     printf("Client Application\n");
 
-    WSADATA wsa;
-    SOCKET sock;
-    struct sockaddr_in server;
+    WSADATA wsa; // Winsock object, required for all applications using winsock
+    SOCKET sock; // socket
+    struct sockaddr_in server; // address object for the server
+
+    // buffers for seding and receiving
     char sendBuffer[MESSAGESIZE];
     char replyBuffer[MESSAGESIZE];
+
+    // Amount of data received
     int recvSize;
 
+    // Starts up winsock
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
     {
         int error = WSAGetLastError();
@@ -32,54 +38,97 @@ int main()
 
     printf("Winsock Initialized!\n");
 
-    if ((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET)
+    // This socket setup is differenct, depending on UDP or TCP
+    if (UDP == 1)
     {
-        int error = WSAGetLastError();
-        printf("Creating a socket failed. Error code: %d\n", error);
-        return error;
+        if ((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == INVALID_SOCKET) // Setup UDP socket
+        {
+            int error = WSAGetLastError();
+            printf("Creating a socket failed. Error code: %d\n", error);
+            return error;
+        }
+    }
+    else
+    {
+        if ((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET) // Setup TCP socket
+        {
+            int error = WSAGetLastError();
+            printf("Creating a socket failed. Error code: %d\n", error);
+            return error;
+        }
     }
 
     printf("Socket Created!\n");
     
+    // Setup server address and port
     //InetPton(AF_INET, (PCWSTR)("127.0.0.1"), &server.sin_addr.s_addr);
-    server.sin_addr.s_addr = inet_addr("192.168.1.32");
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
     server.sin_family = AF_INET;
     server.sin_port = htons(10240);
 
-    if (connect(sock, (struct sockaddr*) &server, sizeof(server)) != 0)
+    // No need to connect in UDP
+    if (UDP == 0)
     {
-        int error = WSAGetLastError();
-        printf("Connecting failed. Error code: %d\n", error);
-        return error;
+        if (connect(sock, (struct sockaddr*) &server, sizeof(server)) != 0) // Connects to TCP server, initial handshake
+        {
+            int error = WSAGetLastError();
+            printf("Connecting failed. Error code: %d\n", error);
+            return error;
+        }
+
+        printf("Connected to server!\n");
     }
 
-    printf("Connected to server!\n");
-
+    // Set string in send buffer
     strcpy_s(sendBuffer, "Hello Sever!");
-    if (send(sock, sendBuffer, strlen(sendBuffer), 0) == 0)
+
+    // Send data to server
+    if (UDP == 1)
     {
-        int error = WSAGetLastError();
-        printf("Sending failed. Error code: %d\n", error);
-        return error;
+        // sento sends a message to a specific IP address, needed for UDP
+        if (sendto(sock, sendBuffer, strlen(sendBuffer), 0, (struct sockaddr*) &server, sizeof(server)) == 0)
+        {
+            int error = WSAGetLastError();
+            printf("Sending failed. Error code: %d\n", error);
+            return error;
+        }
+    }
+    else
+    {
+        // Send will send data on the opened TCP connection
+        if (send(sock, sendBuffer, strlen(sendBuffer), 0) == 0)
+        {
+            int error = WSAGetLastError();
+            printf("Sending failed. Error code: %d\n", error);
+            return error;
+        }
     }
 
     printf("Sent data to server!\n");
 
-    if ((recvSize = recv(sock, replyBuffer, MESSAGESIZE, 0)) == SOCKET_ERROR)
+    // Only listen for a reply from the server if we are TCP
+    if (UDP == 0)
     {
-        int error = WSAGetLastError();
-        printf("Receiving failed. Error code: %d\n", error);
-        return error;
+        // wait for a reply on the same socket for TCP connections
+        if ((recvSize = recv(sock, replyBuffer, MESSAGESIZE, 0)) == SOCKET_ERROR)
+        {
+            int error = WSAGetLastError();
+            printf("Receiving failed. Error code: %d\n", error);
+            return error;
+        }
+
+        // Set the null character in the buffer to limit string
+        replyBuffer[recvSize] = 0;
+
+        printf("Received data! %s\n", replyBuffer);
     }
 
-    replyBuffer[recvSize] = 0;
-
-    printf("Received data! %s\n", replyBuffer);
-
+    // Close socket
     closesocket(sock);
 
     printf("Socket Closed!\n");
 
+    // Shutdown Winsock
     WSACleanup();
 
     return 0;
--- Networking/Server/Server/Server.cpp
+++ Networking/Server/Server/Server.cpp
@@ -6,21 +6,27 @@
 #include <winsock2.h>
 #include <string.h>
 
-#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "ws2_32.lib") // loads the winsock library
 
-#define MESSAGESIZE 1024
+#define UDP 0 // Set this to 1 for UDP, 0 for TCP
+#define MESSAGESIZE 1024 // Resued value for message size
 
 int main()
 {
     printf("Server Application\n");
 
-    WSADATA wsa;
-    SOCKET listenSock, replySock;
-    struct sockaddr_in server, client;
+    WSADATA wsa; // Winsock object, required for all applications using winsock
+    SOCKET listenSock, replySock; // sockets
+    struct sockaddr_in server, client; // address structures for the server to listen and client to send to
+
+    // buffers for seding and receiving
     char sendBuffer[MESSAGESIZE];
     char replyBuffer[MESSAGESIZE];
+
+    // amount of data received
     int recvSize;
 
+    //Initialisze winsock
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
     {
         int error = WSAGetLastError();
@@ -30,15 +36,30 @@ int main()
 
     printf("Winsock Initialized!\n");
 
-    if ((listenSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET)
+    if (UDP == 1)
     {
-        int error = WSAGetLastError();
-        printf("Creating a listening socket failed. Error code: %d\n", error);
-        return error;
+        //Setup UDP socket
+        if ((listenSock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == INVALID_SOCKET)
+        {
+            int error = WSAGetLastError();
+            printf("Creating a listening socket failed. Error code: %d\n", error);
+            return error;
+        }
+    }
+    else
+    {
+        //Setup TCP socket
+        if ((listenSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET)
+        {
+            int error = WSAGetLastError();
+            printf("Creating a listening socket failed. Error code: %d\n", error);
+            return error;
+        }
     }
 
     printf("Listen Socket Created!\n");
 
+    // Set up structure for socket to listen on: listen on any IP address and port 10240
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_family = AF_INET;
     server.sin_port = htons(10240);
@@ -53,47 +74,80 @@ int main()
 
     printf("Binding Complete!\nListening for data!\n");
 
-    listen(listenSock, 3);
-
-    int addr_inSize = sizeof(struct sockaddr_in);
-
-    while ((replySock = accept(listenSock, (struct sockaddr*) &client, &addr_inSize)) != INVALID_SOCKET)
+    if (UDP == 1)
     {
-        printf("Accepted connection!\n");
-        strcpy_s(sendBuffer, "Hello Client!");
-
-        if(send(replySock, sendBuffer, strlen(sendBuffer), 0) == 0)
-        {
-            int error = WSAGetLastError();
-            printf("Sending failed. Error code: %d\n", error);
-            return error;
-        }
-
-        if ((recvSize = recv(replySock, replyBuffer, MESSAGESIZE, 0)) == SOCKET_ERROR)
+        // If we are UDP, just wait for a message to show up
+        if ((recvSize = recv(listenSock, replyBuffer, MESSAGESIZE, 0)) == SOCKET_ERROR)
         {
             int error = WSAGetLastError();
             printf("Receiving failed. Error code: %d\n", error);
             return error;
         }
-
         replyBuffer[recvSize] = 0;
 
         printf("Received data! %s\n", replyBuffer);
-
-        break; // After we have a connection, go ahead and exit loop
     }
-
-    if (replySock == INVALID_SOCKET)
+    else
     {
-        int error = WSAGetLastError();
-        printf("Failed to get a reply socket. Error code: %d\n", error);
-        return error;
+        Listen for TCP connection
+        if (listen(listenSock, 3) == SOCKET_ERROR)
+        {
+            int error = WSAGetLastError();
+            printf("Listening on socket failed. Error code: %d\n", error);
+            return error;
+        }
+
+        int addr_inSize = sizeof(struct sockaddr_in);
+        // Server waits to accept a connection
+        while ((replySock = accept(listenSock, (struct sockaddr*) &client, &addr_inSize)) != INVALID_SOCKET)
+        {
+            printf("Accepted connection!\n");
+
+            strcpy_s(sendBuffer, "Hello Client!");
+
+            // Once we have a connection, say hi to the client
+            if (send(replySock, sendBuffer, strlen(sendBuffer), 0) == 0)
+            {
+                int error = WSAGetLastError();
+                printf("Sending failed. Error code: %d\n", error);
+                return error;
+            }
+
+            // See if the client sends us anything
+            if ((recvSize = recv(replySock, replyBuffer, MESSAGESIZE, 0)) == SOCKET_ERROR)
+            {
+                int error = WSAGetLastError();
+                printf("Receiving failed. Error code: %d\n", error);
+                return error;
+            }
+
+            // Set the end string character so it displays correctly
+            replyBuffer[recvSize] = 0;
+
+            printf("Received data! %s\n", replyBuffer);
+
+            break; // After we have a connection, go ahead and exit loop
+        }
+
+        // If socket is bad, let us know
+        if (replySock == INVALID_SOCKET)
+        {
+            int error = WSAGetLastError();
+            printf("Failed to get a reply socket. Error code: %d\n", error);
+            return error;
+        }
+
+        // Close reply socket
+        closesocket(replySock);
     }
 
+
+    // Close listen socket
     closesocket(listenSock);
 
     printf("Socket Closed!\n");
 
+    // Shutdown Winsock
     WSACleanup();
 
     return 0;
